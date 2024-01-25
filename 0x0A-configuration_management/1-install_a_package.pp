f278482811627e08ea25bef11997f06169790844
lisacharuma <charumalisah@gmail.com>
2024-01-25 08:30:09 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,12 @@
 # installs flask
 
 package {'Werkzeug':
-	ensure	 => '2.1.1',
-	provider => 'pip3'
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
 
 package {'flask':
-	ensure   => '2.1.0',
-	name     => 'flask',
-	provider => 'pip3'
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3'
 }
