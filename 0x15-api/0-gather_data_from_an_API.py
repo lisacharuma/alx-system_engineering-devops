58e87a5c94af49cb739a3004723fd0fd26305458
lisacharuma <charumalisah@gmail.com>
2024-02-19 05:34:54 -0800

    Fixed formatting

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
 """ 0. Gather data from an API """
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    url_todo = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-            argv[1])
-    url_user = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
-    todo_response = requests.get(url_todo)
-    user_response = requests.get(url_user)
-    todo_json = todo_response.json()
-    user_name = user_response.json().get('name')
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
-    done_list = []
-    done_count = 0
-    task_count = 0
-    for i in todo_json:
-        task_count += 1
-        if i.get('completed'):
-            done_list.append(i.get('title'))
-            done_count += 1
+    response = requests.get(url)
+    employeeName = response.json().get('name')
 
-    td = "{}/{}".format(done_count, task_count)
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
 
-    print("Employee {} is done with tasks({}):".format(user_name, td))
-    for task in done_list:
-        print('\t {}'.format(task))
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
